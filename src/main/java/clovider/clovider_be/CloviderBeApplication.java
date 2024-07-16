cc2095d9a331de43dfa6d53c64cad7b4faa99ca1
mango0422_mac <syj0422@gachon.ac.kr>
2024-07-16 16:14:10 +0900

    first commit

---
+++ src/main/java/clovider/clovider_be/CloviderBeApplication.java
@@ -0,0 +1,13 @@
+package clovider.clovider_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloviderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloviderBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clovider/clovider_be/CloviderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package clovider.clovider_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloviderBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
